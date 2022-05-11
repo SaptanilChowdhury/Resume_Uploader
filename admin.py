65174dbf66ac8194a285168bad77910623e8a7da
Saptanil Chowdhury <104817362+SaptanilChowdhury@users.noreply.github.com>
2022-05-11 11:02:10 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Resume1
-
-# Register your models here.
-@admin.register(Resume1)
-class ResumeModelAdmin(admin.ModelAdmin):
-    list_display = ['id','name','dob','gender','locality','city','pin','state','mobile','job_city','profile_image','my_file']
